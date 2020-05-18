78bc624b2ee0cd2fd6b0c3fc9dc40668f0f4a500
Nills <nills.van.limbeek@live.nl>
2020-05-18 10:43:41 +0200

    environment variables

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,12 +1,11 @@
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
-// TODO .env file
 const firebaseConfig = {
-    apiKey: 'AIzaSyCXxtZO1hJDFih_Y6HDiKpTWUC3RCfFwWQ',
-    authDomain: 'learning-tool-22eb6.firebaseapp.com',
-    databaseURL: 'https://learning-tool-22eb6.firebaseio.com',
-    projectId: 'learning-tool-22eb6',
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
