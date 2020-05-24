52acdb70b8204827a88b16c0ea287603e893104b
Nills <nills.van.limbeek@live.nl>
2020-05-24 19:14:55 +0200

    added users

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,22 +1,22 @@
-import { Request, Response, NextFunction, Router } from 'express';
+import { Router } from 'express';
 
 import User from '../models/User';
 
 const router = Router();
 
-router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/', async (req, res, next) => {
     const users = await User.find();
 
     res.status(200).json({ data: users });
 });
 
-router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/:id', async (req, res, next) => {
     const user = await User.findById(req.params.id);
 
     res.status(200).json({ data: user });
 });
 
-router.post('/', async (req: Request, res: Response, next: NextFunction) => {
+router.post('/', async (req, res, next) => {
     const user = await User.create(req.body);
 
     res.status(200).json({ data: user });
